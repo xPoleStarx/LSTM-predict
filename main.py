fcc35624414317b9aa8533790d58621261ac7da4
Seyfullah Korkmaz <seyfullahkorkmaz115@gmail.com>
2024-06-29 18:32:07 +0300

    lstmpredictor model added

--- main.py
+++ main.py
@@ -21,3 +21,38 @@ plt.yticks(fontsize=10)
 plt.plot(np.arange(x.shape[1]), y[0, :], 'r', linewidth=2.0)
 
 plt.show()
+
+
+class LSTMPredictor(nn.Module):
+    def __init__(self, n_hidden=51):
+        super(LSTMPredictor, self).__init__()
+        self.n_hidden = n_hidden
+        # lstm1,  lstm2, lineer
+        self.lstm1 = nn.LSTMCell(1, self.n_hidden)
+        self.lstm2 = nn.LSTMCell(self.n_hidden, self.n_hidden)
+        self.linear = nn.Linear(self.n_hidden, 1)
+
+    def forward(self, x, future=0):
+        # N, 100
+        outputs = []
+        n_samples = x.size(0)
+        h_t = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+        c_t = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+        h_t2 = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+        c_t2 = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+
+        for input_t in x.split(1, dim=1):
+            # N, 1
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2))
+            output = self.linear(h_t2)
+            outputs.append(output)
+
+        for i in range(future):
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t, c_t))
+            output = self.linear(h_t2)
+            outputs.append(output)
+
+        outputs = torch.cat(outputs, dim=1)
+        return outputs
